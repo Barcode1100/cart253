758d5b613e46699227f0a8a5210c43df90e1e33b
Barcode1100 <Sawyerpower9@gmail.com>
2026-09-22 10:29:33 -0400

    Created House
    Beautiful House was created into exsistence, many errors rectified.

--- topics/House-Project/js/script.js
+++ topics/House-Project/js/script.js
@@ -8,41 +8,57 @@ function setup() {
 }
 
 function draw() {
-    background(0, 0, 255);
-    drawSun();
-    drawGround();
-    drawEar();
+    background(114, 213, 255);
+    drawSquare();
+    drawTriangle();
+    drawRect();
+    drawWindow();
+    drawWin();
+    
 }
 
-function drawSun() {
+function drawSquare() {
     push();
     fill(225,225,0)
-    ellipse(200,250,300,300);
+    rectMode(CENTER) 
+    square(200,300,200)
+  
     pop();
 
 }
 
-function drawGround() {
+function drawTriangle() {
     push();
-    fill(0,225,0)
-    rect(0,300,400,300)
+    fill(225,0,0) 
+    triangle(325,200,200,100,75,200)
+  
     pop();
+
 }
 
-function drawEar() {
+function drawRect() {
     push();
-    fill(200,225,0)
-    triangle(100,200,200,200,0,0)
+    fill(225,100,50) 
+    rect(175,320,50,80)
+  
     pop();
 
+}
+
+function drawWindow() {
     push();
-    fill(225,225,0)
-    triangle(200,100,200,200,0,0)
+    fill(20,100,225) 
+    square(125,240,50)
+  
     pop();
 
+}
+
+function drawWin() {
     push();
-    noStroke();
-    fill("#4d873b")
-    triangle(100,300,200,300,0,400)
+    fill(20,100,225) 
+    square(225,240,50)
+  
     pop();
+
 }
