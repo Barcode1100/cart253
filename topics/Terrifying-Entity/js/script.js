ae209ffe36993db033ba267d54b7dfb548bfed39
Barcode1100 <Sawyerpower9@gmail.com>
2026-09-23 20:05:47 -0400

    Its even scarier!
    each shape now trembles which makes it even freakier...

--- topics/Terrifying-Entity/js/script.js
+++ topics/Terrifying-Entity/js/script.js
@@ -24,8 +24,11 @@ function drawBody() {
 
     push();
 
+    let x = random(-2,2);
+    let y = random(-2,2);
+
     fill(20,20,20);
-    ellipse(200,280,170,300);
+    ellipse(200+x,280+y,170,300);
 
     pop();
 
@@ -35,21 +38,28 @@ function drawHead() {
 
     push();
 
+    let x = random(-3,3);
+    let y = random(-3,3);
+
     fill(10,10,10);
-    ellipse(200,100,90,100);
+    ellipse(200+x,100+y,90,100);
 
     pop();
 
 }
+
 function drawArms() {
 
     push();
 
+    let x = random(-3,3);
+    let y = random(-3,3);
+
     stroke(10,10,10);
     strokeWeight(20);
 
-    line(135,200,40,390);
-    line(265,200,360,390);
+    line(135+x,200+y,40+x,390+y);
+    line(265+x,200+y,360+x,390+y);
 
     pop();
 
@@ -59,11 +69,36 @@ function drawEyes() {
 
     push();
 
+    let x = random(-5,5);
+    let y = random(-5,5);
+
     noStroke();
     fill(255,255,255);
 
-    ellipse(185,95,12,25);
-    ellipse(220,105,12,25);
+    ellipse(185+x,95+y,12,25);
+    ellipse(220+x,105+y,12,25);
+
+    pop();
+
+}
+
+function drawMouth() {
+
+    push();
+
+    let x = random(-2,2);
+    let y = random(-2,2);
+
+    noStroke();
+    fill(0,0,0);
+
+    ellipse(200+x,135+y,65,45);
+
+    fill(255,255,255);
+
+    triangle(170+x,120+y,180+x,145+y,190+x,120+y);
+    triangle(190+x,120+y,200+x,150+y,210+x,120+y);
+    triangle(210+x,120+y,220+x,145+y,230+x,120+y);
 
     pop();
 
